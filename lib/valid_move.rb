ca1244895d59054b859eebfa4bc49cf8dec6e5af
Victor Torres <torres.victor136@gmail.com>
2020-07-08 19:23:01 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
   def position_taken?(board, index)
     if board [index] == " " || board[index] == "" || board[index] == nil
       return false
-    else board[index] == "x" || board[index] == "O"
+    else 
       return true
     end
   end
